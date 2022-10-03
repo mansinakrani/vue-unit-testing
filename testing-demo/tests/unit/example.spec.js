92259edf4fdb21de32108dd5d9276753596d20da
mansinakrani <mansinakrani006@gmail.com>
2022-10-03 18:27:50 +0530

    test structure

--- testing-demo/tests/unit/example.spec.js
+++ testing-demo/tests/unit/example.spec.js
@@ -1,12 +1,16 @@
 import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
+//Tests Block
 describe('HelloWorld.vue', () => {
+  //Test Block
   it('renders props.msg when passed', () => {
     const msg = 'new message'
+    //Our component
     const wrapper = shallowMount(HelloWorld, {
       props: { msg }
     })
+    //Assertion
     expect(wrapper.text()).toMatch(msg)
   })
 })
