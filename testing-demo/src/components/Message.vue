ef3ad8c5817ce9d74366730833e2bfa671348282
mansinakrani <mansinakrani006@gmail.com>
2022-10-04 15:44:40 +0530

    first test

---
+++ testing-demo/src/components/Message.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h1>{{ msg }}</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Message',
+    props: {
+      msg: String
+    },
+  };
+  </script>
+  
