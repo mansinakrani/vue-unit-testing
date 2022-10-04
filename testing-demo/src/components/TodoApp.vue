391761e49d60d42d6ee9ce6883d502bc6d9f7271
mansinakrani <mansinakrani006@gmail.com>
2022-10-04 17:39:27 +0530

    todo app component added(finding elements for test)

---
+++ testing-demo/src/components/TodoApp.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div v-for="todo in todos" :key="todo.id" data-test="todo">
+            {{ todo.text }}
+        </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'TodoApp',
+    data() {
+        return {
+            todos: [
+                {
+                    id: 1,
+                    text: "Learn Vue Testing",
+                    completed: false,
+                }
+            ]
+        }
+    }
+  };
+  </script>
+  
