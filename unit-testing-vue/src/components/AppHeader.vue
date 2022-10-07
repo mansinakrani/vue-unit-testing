a7fa675b4f44bedfe2e0850de088efa149b85cc2
mansinakrani <mansinakrani006@gmail.com>
2022-10-07 17:03:29 +0530

    writing a unit test with jest

---
+++ unit-testing-vue/src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- Output => Input false : no output, Input true: output-->
+    <button v-show="loggedIn">Logout</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loggedIn: false, //Input
+    };
+  },
+};
+</script>
